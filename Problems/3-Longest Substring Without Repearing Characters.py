ba7d969c0cb435da086331b4260029a03efd8d81
NumiKun <nugrohorizki20@gmail.com>
2026-08-13 10:35:53 +0700

    feat: add sliding window solution for Longest Substring Without Repeating Characters

---
+++ Problems/3-Longest Substring Without Repearing Characters.py
@@ -0,0 +1,44 @@
+# Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+# Constraints:
+
+# 0 <= s.length <= 105
+# s consists of English letters, digits, symbols and spaces.
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_map = {}
+        left = 0
+        max_len = 0
+
+        for right, char in enumerate(s):
+            if char in char_map and char_map[char] >= left:
+                left = char_map[char] + 1
+            char_map[char] = right
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
