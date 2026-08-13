81d5ebf72a5806c75dac78c6f76147b39f17d43d
NumiKun <nugrohorizki20@gmail.com>
2026-08-13 10:28:36 +0700

    feat: edit class ListNode

--- Problems/2-Add Two Numbers.py
+++ Problems/2-Add Two Numbers.py
@@ -27,10 +27,10 @@
 # It is guaranteed that the list represents a number that does not have leading zeros.
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
